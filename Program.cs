d56de198fd58a86539cd8fd52f956a2d555e01dd
justin long <justinxlong93@gmail.com>
2025-01-30 15:52:52 -0600

    Exercise complete

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace LinqExercise2
     {
         static void Main(string[] args)
         {
+            List<string> videoGames = new List<string>() 
+            {
+                "Old School Runescape",
+                "Red Dead Redemption 2",
+                "The Elder Scrolls Online",
+                "Super Mario",
+            };
 
+            var orderedGameByLength = videoGames.OrderBy(x => x.Length).ToList();
+
+            Console.WriteLine("List of video games ordered by length of name:");
+            foreach (var game in orderedGameByLength)
+            {
+                Console.WriteLine($"- {game}");
+            }
         }
     }
 }
